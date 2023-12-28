41577ce1ea83f9bf0ead09815d01f4060e3f656e
Jon Mease <jonmmease@gmail.com>
2023-12-28 14:45:57 -0500

    Add initial rule shader

--- vega-wgpu-renderer/src/scene/mod.rs
+++ vega-wgpu-renderer/src/scene/mod.rs
@@ -1,3 +1,4 @@
 pub mod rect;
+pub mod rule;
 pub mod scene_graph;
 pub mod symbol;
--- vega-wgpu-renderer/src/specs/mod.rs
+++ vega-wgpu-renderer/src/specs/mod.rs
@@ -2,6 +2,7 @@ pub mod dims;
 pub mod group;
 pub mod mark;
 pub mod rect;
+pub mod rule;
 pub mod symbol;
 
 use crate::specs::group::GroupItemSpec;
