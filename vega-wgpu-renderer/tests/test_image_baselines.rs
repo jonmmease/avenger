a34f52dd33c8d457637abff235b2fcbf38721643
Jon Mease <jonmmease@gmail.com>
2023-12-28 13:11:53 -0500

    Don't cut off anti-aliased border

--- vega-wgpu-renderer/tests/test_image_baselines.rs
+++ vega-wgpu-renderer/tests/test_image_baselines.rs
@@ -27,7 +27,7 @@ mod test_image_baselines {
         case("symbol", "binned_scatter_wedge", 0.009),
         case("symbol", "binned_scatter_arrow", 0.009),
         case("symbol", "binned_scatter_cross", 0.01),
-        case("symbol", "binned_scatter_circle", 0.002)
+        case("symbol", "binned_scatter_circle", 0.0004)
     )]
     fn test_image_baseline(category: &str, spec_name: &str, tolerance: f64) {
         let specs_dir = format!("{}/tests/specs/{category}", env!("CARGO_MANIFEST_DIR"));
