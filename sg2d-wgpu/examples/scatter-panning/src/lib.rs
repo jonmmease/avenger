ea1c436e9718bb77127e265fbeb43f7ce86b62bd
Jon Mease <jonmmease@gmail.com>
2024-01-09 20:30:29 -0500

    Add panning scatterplot example for performance testing

--- sg2d-wgpu/examples/scatter-panning/src/lib.rs
+++ sg2d-wgpu/examples/scatter-panning/src/lib.rs
@@ -1,9 +1,15 @@
+use rand::Rng;
 use sg2d::scene_graph::SceneGraph;
 use sg2d_vega::dims::VegaSceneGraphDims;
 use sg2d_vega::scene_graph::VegaSceneGraph;
 use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+use sg2d::marks::group::{GroupBounds, SceneGroup};
+use sg2d::marks::mark::SceneMark;
+use sg2d::marks::symbol::{SymbolMark, SymbolShape};
+use sg2d::value::EncodingValue;
+use sg2d_vega::marks::symbol::shape_to_path;
 use sg2d_wgpu::canvas::{Canvas, WindowCanvas};
 
 #[cfg(target_arch = "wasm32")]
@@ -41,27 +47,33 @@ pub async fn run() {
             })
             .expect("Couldn't append canvas to document body.");
     }
-    // Load scene graph
-    let scene_spec: VegaSceneGraph = serde_json::from_str(include_str!(
-        "../../../../sg2d-vega-test-data/vega-scenegraphs/symbol/binned_scatter_cross_stroke.sg.json"
-    ))
-        .unwrap();
-
-    // Load dims
-    let scene_dims: VegaSceneGraphDims = serde_json::from_str(include_str!(
-        "../../../../sg2d-vega-test-data/vega-scenegraphs/symbol/binned_scatter_cross_stroke.dims.json"
-    ))
-        .unwrap();
 
     // Extract dims and set window size
-    let origin = [scene_dims.origin_x, scene_dims.origin_y];
-    let width = scene_dims.width;
-    let height = scene_dims.height;
-    let scale = 2.0;
+    let mut rng = rand::thread_rng();
+    let origin = [20.0, 20.0];
+    let inner_width = 400.0;
+    let inner_height = 400.0;
+    let margin = 20.0;
+    let width = inner_width + 2.0 * margin;
+    let height = inner_height + 2.0 * margin;
+    let scale = 1.0;
 
-    let scene_graph: SceneGraph = scene_spec
-        .to_scene_graph(origin, width, height)
-        .expect("Failed to parse scene graph");
+    let shape = shape_to_path("circle").unwrap();
+
+    let mut x: Vec<f32> = Vec::new();
+    let mut y: Vec<f32> = Vec::new();
+    let mut fill: Vec<[f32; 4]> = Vec::new();
+    let mut size: Vec<f32> = Vec::new();
+
+    let n = 10000;
+    for _ in 0..n {
+        x.push(rng.gen::<f32>() * inner_width + margin);
+        y.push(rng.gen::<f32>() * inner_height + margin);
+        size.push(rng.gen::<f32>() * 300.0 + 100.0);
+        fill.push([0.5, 0.5, rng.gen::<f32>(), 0.4]);
+    }
+
+    let scene_graph = make_sg(width, height, &shape, &x, &y, &fill, &size, 0.0, 0.0);
 
     // Save to png
     let mut canvas = WindowCanvas::new(window, width, height, scale)
@@ -96,6 +108,17 @@ pub async fn run() {
                             // new_inner_size is &&mut so w have to dereference it twice
                             canvas.resize(**new_inner_size);
                         }
+                        WindowEvent::CursorMoved { device_id, position, modifiers } => {
+                            // println!("position: {position:?}");
+                            let scene_graph = make_sg(
+                                width, height, &shape, &x, &y, &fill, &size, position.x as f32 - 100.0, position.y as f32 - 100.0
+                            );
+                            canvas.set_scene(&scene_graph).unwrap();
+                            canvas.window().request_redraw();
+                        }
+                        WindowEvent::MouseInput { device_id, state, button, modifiers } => {
+                            // println!("state: {state:?}, button: {button:?}");
+                        }
                         _ => {}
                     }
                 }
@@ -122,4 +145,41 @@ pub async fn run() {
             _ => {}
         }
     });
+}
+
+fn make_sg(width: f32, height: f32, shape: &SymbolShape, x: &[f32], y: &[f32], fill: &[[f32; 4]], size: &[f32], x_offset: f32, y_offset: f32) -> SceneGraph {
+    let x: Vec<f32> = x.iter().map(|v| *v + x_offset).collect();
+    let y: Vec<f32> = y.iter().map(|v| *v + y_offset).collect();
+    let fill: Vec<[f32; 4]> = Vec::from(fill);
+    let size: Vec<f32> = Vec::from(size);
+
+    SceneGraph {
+        groups: vec![
+            SceneGroup {
+                bounds: GroupBounds {
+                    x: 0.0,
+                    y: 0.0,
+                    width: None,
+                    height: None,
+                },
+                marks: vec![
+                    SceneMark::Symbol(SymbolMark {
+                        name: "scatter".to_string(),
+                        clip: false,
+                        shape: shape.clone(),
+                        stroke_width: None,
+                        len: x.len() as u32,
+                        x: EncodingValue::Array { values: x },
+                        y: EncodingValue::Array { values: y },
+                        fill: EncodingValue::Array { values: fill },
+                        size: EncodingValue::Array { values: size },
+                        stroke: EncodingValue::Scalar { value: [0.0, 0.0, 0.0, 0.0] },
+                        angle: EncodingValue::Scalar { value: 0.0 },
+                    })
+                ],
+            }
+        ],
+        width,
+        height,
+    }
 }
