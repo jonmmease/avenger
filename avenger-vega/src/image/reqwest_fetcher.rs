7f9b3d6e324ead3deb773413c210987079841a2b
Jon Mease <jonmmease@gmail.com>
2024-01-31 07:49:36 -0500

    Support loading svg images with resvg

--- avenger-vega/src/image/reqwest_fetcher.rs
+++ avenger-vega/src/image/reqwest_fetcher.rs
@@ -3,6 +3,9 @@ use crate::image::ImageFetcher;
 use image::DynamicImage;
 use reqwest::blocking::{Client, ClientBuilder};
 
+#[cfg(feature = "svg")]
+use crate::image::svg::svg_to_png;
+
 pub struct ReqwestImageFetcher {
     client: Client,
 }
@@ -28,7 +31,20 @@ impl ReqwestImageFetcher {
 
 impl ImageFetcher for ReqwestImageFetcher {
     fn fetch_image(&self, url: &str) -> Result<DynamicImage, AvengerVegaError> {
-        let img_data = self.client.get(url).send()?.bytes()?.to_vec();
+        let img_data = if url.ends_with(".svg") {
+            cfg_if::cfg_if! {
+                if #[cfg(feature = "svg")] {
+                    let svg_str = self.client.get(url).send()?.text()?;
+                    svg_to_png(&svg_str, 2.0)?
+                } else {
+                    return Err(AvengerVegaError::SvgSupportDisabled(
+                        "Fetching SVG images requires the svg feature flag".to_string()
+                    ))
+                }
+            }
+        } else {
+            self.client.get(url).send()?.bytes()?.to_vec()
+        };
         let diffuse_image = image::load_from_memory(img_data.as_slice())?;
         Ok(diffuse_image)
     }
