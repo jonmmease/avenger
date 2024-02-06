b52745a74d6bf89f5de45e668abe1bdaebb39566
Jon Mease <jonmmease@gmail.com>
2024-02-05 19:41:09 -0500

    Get path gradients working in multi mark renderer

--- avenger-wgpu/src/marks/mod.rs
+++ avenger-wgpu/src/marks/mod.rs
@@ -3,11 +3,12 @@ pub mod basic_mark;
 pub mod gradient;
 pub mod image;
 pub mod instanced_mark;
+pub mod multi;
 pub mod path;
 pub mod rect;
 pub mod rule;
 pub mod symbol;
-pub mod multi;
 
+mod gradient2;
 #[cfg(feature = "text-glyphon")]
 pub mod text;
