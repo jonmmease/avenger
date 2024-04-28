50041faf8e46942c45ba74df0e9a3dd1f0fc5c55
Jon Mease <jonmmease@gmail.com>
2024-04-28 17:26:12 -0400

    Support line legends

--- avenger-vega-renderer/js/marks/symbol.js
+++ avenger-vega-renderer/js/marks/symbol.js
@@ -1,10 +1,13 @@
-import {SymbolMark} from "../../pkg/avenger_wasm.js";
+import {SymbolMark, GroupMark, LineMark} from "../../pkg/avenger_wasm.js";
 import {encodeSimpleArray} from "./util.js";
 
 
 /**
  * @typedef {Object} SymbolItem
  * @property {number} strokeWidth
+ * @property {"butt"|"round"|"square"} strokeCap
+ * @property {"bevel"|"miter"|"round"} strokeJoin
+ * @property {string|number[]} strokeDash
  * @property {string|object} fill
  * @property {string|object} stroke
  * @property {number} x
@@ -48,13 +51,6 @@ export function importSymbol(vegaSymbolMark, force_clip) {
     }
 
     const firstItem = items[0];
-    const firstShape = firstItem.shape ?? "circle";
-
-    if (firstShape === "stroke") {
-        // TODO: Handle line legends
-        return symbolMark
-    }
-
     // Only include stroke_width if there is a stroke color
     const firstHasStroke = firstItem.stroke != null;
     let strokeWidth;
@@ -169,4 +165,53 @@ export function importSymbol(vegaSymbolMark, force_clip) {
     }
 
     return symbolMark;
+}
+
+
+/**
+ * Handle special case of symbols with shape == "stroke". This happens when lines are
+ * sed in legends. We convert these to a group of regular line marks
+ * @param {SymbolMarkSpec} vegaSymbolMark
+ * @param {boolean} force_clip
+ * @returns {GroupMark}
+ */
+export function importStrokeLegend(vegaSymbolMark, force_clip) {
+    const groupMark = new GroupMark(
+        0, 0, "symbol_line_legend", undefined, undefined
+    );
+
+    for (let item of vegaSymbolMark.items) {
+        let width = Math.sqrt(item.size ?? 100.0);
+        let x = item.x ?? 0;
+        let y = item.y ?? 0;
+        const lineMark = new LineMark(
+            2, vegaSymbolMark.clip || force_clip, undefined, vegaSymbolMark.zindex
+        );
+
+        lineMark.set_xy(
+            new Float32Array([x - width / 2.0, x + width / 2.0]),
+            new Float32Array([y, y])
+        )
+
+        lineMark.set_stroke(item.stroke ?? "", (item.opacity ?? 1) * (item.strokeOpacity ?? 1));
+        if (item.strokeWidth != null) {
+            lineMark.set_stroke_width(item.strokeWidth);
+        }
+
+        if (item.strokeCap != null) {
+            lineMark.set_stroke_cap(item.strokeCap);
+        }
+
+        if (item.strokeJoin != null) {
+            lineMark.set_stroke_join(item.strokeJoin);
+        }
+
+        if (item.strokeDash != null) {
+            lineMark.set_stroke_dash(item.strokeDash);
+        }
+
+        groupMark.add_line_mark(lineMark);
+    }
+
+    return groupMark
 }
