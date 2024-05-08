6544a0c44543eebb4d472f79c1c61556fbdc674d
Jon Mease <jonmmease@gmail.com>
2024-05-08 08:56:13 -0400

    Force WebGL backend in browser

--- avenger-wgpu/src/html_canvas.rs
+++ avenger-wgpu/src/html_canvas.rs
@@ -35,7 +35,10 @@ impl<'window> HtmlCanvasCanvas<'window> {
     ) -> Result<Self, AvengerWgpuError> {
         canvas.set_width(dimensions.to_physical_width());
         canvas.set_height(dimensions.to_physical_height());
-        let instance = make_wgpu_instance();
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::GL,
+            ..Default::default()
+        });
         let surface = instance.create_surface(SurfaceTarget::Canvas(canvas))?;
         let adapter = make_wgpu_adapter(&instance, Some(&surface)).await?;
         let (device, queue) = request_wgpu_device(&adapter).await?;
