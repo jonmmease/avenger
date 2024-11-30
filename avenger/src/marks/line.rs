25e0251cc31713b3423b0810fd5dec4f9ee7d066
Jon Mease <jonmmease@gmail.com>
2024-11-30 18:21:15 -0500

    Instance refactor

--- avenger/src/marks/line.rs
+++ avenger/src/marks/line.rs
@@ -1,4 +1,5 @@
 use crate::marks::value::{ColorOrGradient, EncodingValue, Gradient, StrokeCap, StrokeJoin};
+use itertools::izip;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -8,47 +9,77 @@ pub struct LineMark {
     pub clip: bool,
     pub len: u32,
     pub gradients: Vec<Gradient>,
-    pub x: EncodingValue<f32>,
-    pub y: EncodingValue<f32>,
-    pub defined: EncodingValue<bool>,
     pub stroke: ColorOrGradient,
     pub stroke_width: f32,
     pub stroke_cap: StrokeCap,
     pub stroke_join: StrokeJoin,
     pub stroke_dash: Option<Vec<f32>>,
     pub zindex: Option<i32>,
+
+    // Encodings
+    pub x: EncodingValue<f32>,
+    pub y: EncodingValue<f32>,
+    pub defined: EncodingValue<bool>,
 }
 
 impl LineMark {
-    pub fn x_iter(&self) -> Box<dyn Iterator<Item = &f32> + '_> {
-        self.x.as_iter(self.len as usize, None)
-    }
-
-    pub fn y_iter(&self) -> Box<dyn Iterator<Item = &f32> + '_> {
-        self.y.as_iter(self.len as usize, None)
-    }
-
-    pub fn defined_iter(&self) -> Box<dyn Iterator<Item = &bool> + '_> {
-        self.defined.as_iter(self.len as usize, None)
+    pub fn instances(&self) -> Box<dyn Iterator<Item = LineMarkInstance> + '_> {
+        let n = self.len as usize;
+        Box::new(
+            izip!(
+                self.x.as_iter(n, None),
+                self.y.as_iter(n, None),
+                self.defined.as_iter(n, None)
+            )
+            .map(|(x, y, defined)| LineMarkInstance {
+                x: *x,
+                y: *y,
+                defined: *defined,
+            }),
+        )
     }
 }
 
 impl Default for LineMark {
     fn default() -> Self {
+        let default_instance = LineMarkInstance::default();
         Self {
             name: "line_mark".to_string(),
             clip: true,
             len: 1,
             gradients: vec![],
-            x: EncodingValue::Scalar { value: 0.0 },
-            y: EncodingValue::Scalar { value: 0.0 },
-            defined: EncodingValue::Scalar { value: true },
+            x: EncodingValue::Scalar {
+                value: default_instance.x,
+            },
+            y: EncodingValue::Scalar {
+                value: default_instance.y,
+            },
+            defined: EncodingValue::Scalar {
+                value: default_instance.defined,
+            },
             stroke: ColorOrGradient::Color([0.0, 0.0, 0.0, 1.0]),
             stroke_width: 1.0,
             stroke_cap: Default::default(),
             stroke_join: Default::default(),
             stroke_dash: None,
             zindex: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LineMarkInstance {
+    pub x: f32,
+    pub y: f32,
+    pub defined: bool,
+}
+
+impl Default for LineMarkInstance {
+    fn default() -> Self {
+        Self {
+            x: 0.0,
+            y: 0.0,
+            defined: true,
         }
     }
 }
