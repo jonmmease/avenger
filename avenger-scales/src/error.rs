7861ebcf1e7fece690cf79c70ebb1778642c0180
Jon Mease <jonmmease@gmail.com>
2024-12-02 12:05:35 -0500

    Add threshold scale

--- avenger-scales/src/error.rs
+++ avenger-scales/src/error.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, PartialEq, thiserror::Error)]
 pub enum AvengerScaleError {
     #[error("Domain length ({domain_len}) does not match range length ({range_len})")]
     DomainRangeMismatch { domain_len: usize, range_len: usize },
@@ -12,6 +12,14 @@ pub enum AvengerScaleError {
     #[error("Bins must be in ascending order: {0:?}")]
     BinsNotAscending(Vec<f32>),
 
+    #[error("Thresholds must be in ascending order: {0:?}")]
+    ThresholdsNotAscending(Vec<f32>),
+
     #[error("Domain value not found: {0}")]
     DomainValueNotFound(String),
+
+    #[error(
+        "Threshold domain length ({domain_len}) must be one less than range length ({range_len})"
+    )]
+    ThresholdDomainMismatch { domain_len: usize, range_len: usize },
 }
