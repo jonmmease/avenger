5c519d6ef3ba285167068c8fc55963c4f044387d
Jon Mease <jonmmease@gmail.com>
2024-12-09 16:38:48 -0500

    accept generic point and hand scales for axis construction

--- avenger-guides/src/axis/point.rs
+++ avenger-guides/src/axis/point.rs
@@ -1,13 +1,17 @@
 use super::{band::make_band_axis_marks, opts::AxisConfig};
 use avenger_scales::point::PointScale;
 use avenger_scenegraph::marks::group::SceneGroup;
+use std::{fmt::Debug, hash::Hash};
 
-pub fn make_point_axis_marks(
-    scale: &PointScale<String>,
+pub fn make_point_axis_marks<T>(
+    scale: &PointScale<T>,
     title: &str,
     origin: [f32; 2],
     config: &AxisConfig,
-) -> SceneGroup {
+) -> SceneGroup
+where
+    T: ToString + Debug + Clone + Hash + Eq + Sync + 'static,
+{
     let band_scale = scale.clone().to_band();
     make_band_axis_marks(&band_scale, title, origin, config)
 }
