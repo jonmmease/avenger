3cc15b80488314325cf3f417aee36c49dcfbc3ef
Jon Mease <jonmmease@gmail.com>
2024-12-12 20:30:05 -0500

    simplify to just use throttle

--- avenger-eventstream/src/lib.rs
+++ avenger-eventstream/src/lib.rs
@@ -4,7 +4,7 @@ pub mod window;
 
 pub use scene::{SceneGraphEvent, SceneGraphEventType};
 pub use window::{
-    WindowEvent, ElementState, Key, MouseButton, MouseScrollDelta, NamedKey, TouchPhase,
-    WindowCursorMoved, WindowKeyboardInput, WindowMouseInput, WindowMouseWheel, WindowMovedEvent,
+    ElementState, Key, MouseButton, MouseScrollDelta, NamedKey, TouchPhase, WindowCursorMoved,
+    WindowEvent, WindowKeyboardInput, WindowMouseInput, WindowMouseWheel, WindowMovedEvent,
     WindowResizeEvent, WindowTouch,
 };
