f42482ced0bad1781c45483f3e26d056977f9b6e
Jon Mease <jonmmease@gmail.com>
2025-05-01 20:31:04 -0400

    cli updates

--- avenger-lang2/src/imports.rs
+++ avenger-lang2/src/imports.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashMap, ops::ControlFlow};
+use std::{collections::HashMap, ops::ControlFlow, path::PathBuf};
 
 use sqlparser::ast::{Ident, VisitorMut};
 
-use crate::{ast::{AvengerFile, ComponentProp, ImportStatement, Statement}, error::AvengerLangError, loader::AvengerLoader, visitor::{AvengerVisitorMut, VisitorContext}};
+use crate::{ast::{AvengerFile, ComponentProp, ImportStatement, Statement}, error::AvengerLangError, loader::{AvengerFilesystemLoader, AvengerLoader}, visitor::{AvengerVisitorMut, VisitorContext}};
 
 use std::sync::Arc;
 
@@ -54,6 +54,8 @@ impl AvengerVisitorMut for InlineImportsVisitor {
     fn pre_visit_component_prop(&mut self, component_prop: &mut ComponentProp, _context: &VisitorContext) -> ControlFlow<Self::Break> {
         let component_name = &component_prop.component_type.value;
         if let Some(file_ast) = self.imports.get(component_name) {
+            // TODO: replace @root references in file_ast with @{component_prop.prop_name}
+
             // Collect my binding properties
             let my_bindings = component_prop.prop_bindings();
 
@@ -136,6 +138,10 @@ impl AvengerVisitorMut for InlineImportsVisitor {
         file.statements.retain(|statement| {
             !matches!(statement, Statement::Import(_))
         });
+
+        // Change the component type to Group
+        file.name = "Group".to_string();
+        
         ControlFlow::Continue(())
     }
 
@@ -147,6 +153,37 @@ impl AvengerVisitorMut for InlineImportsVisitor {
         ControlFlow::Continue(())
     }
 }
+
+
+/// Load and parse a main component file from a project, inlining all imports.
+/// path is the path to the main component file, and this file is assumed to be under the 
+/// root of the project.
+pub fn load_main_component_file(path: PathBuf, verbose: bool) -> Result<AvengerFile, AvengerLangError> {
+    let path = path.canonicalize()?;
+    let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
+    let Some(component_type) = file_name.strip_suffix(".avgr") else {
+        return Err(AvengerLangError::InvalidFileExtensionError(path));
+    };
+
+    let Some(base_path) = path.parent() else {
+        return Err(AvengerLangError::InternalError(
+            format!("Failed to get parent of path: {:?}", path))
+        );
+    };
+
+    let loader = Arc::new(
+        AvengerFilesystemLoader::new(base_path, verbose)
+    );
+
+    let mut main_component = loader.load_file(component_type, ".")?;
+    let mut visitor = InlineImportsVisitor::new(loader);
+    if let ControlFlow::Break(Err(e)) = main_component.visit_mut(&mut visitor) {
+        return Err(e);
+    };
+
+    Ok(main_component)
+}
+
 
 #[cfg(test)]
 mod tests {
