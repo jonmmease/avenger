fb0b323d0ac99bb98b119075c29a7e5fea97c66e
Jon Mease <jonmmease@gmail.com>
2025-08-07 20:32:41 -0700

    Remove layout stub types

--- examples/wgpu-scales/src/util.rs
+++ examples/wgpu-scales/src/util.rs
@@ -326,6 +326,7 @@ pub async fn run() {
             colorbar_width: None,
             colorbar_height: None,
             colorbar_margin: Some(8.0),
+            left_padding: None,
         },
     )
     .unwrap();
