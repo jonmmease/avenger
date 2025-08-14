4e5baacadf18bfdb4384ef65cbd27069357e8dd1
Jon Mease <jonmmease@gmail.com>
2025-08-14 18:55:01 -0400

    review

--- avenger-chart/src/lib.rs
+++ avenger-chart/src/lib.rs
@@ -4,7 +4,7 @@ mod chart_layout;
 pub mod constants;
 pub mod coords;
 pub mod error;
-mod legend;
+pub mod legend;
 pub mod marks;
 pub mod plot;
 mod plot_legends;
@@ -12,9 +12,6 @@ mod plot_scales;
 pub mod render;
 pub mod scales;
 pub mod utils;
-
-// Re-export selected types for external tests and users
-pub use crate::legend::LegendPosition;
 
 #[cfg(test)]
 pub mod test_utils;
