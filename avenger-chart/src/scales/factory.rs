bbb0b1490ed0401e970e008db1ef2b0d0ac9d41c
Jon Mease <jonmmease@gmail.com>
2025-09-04 19:42:19 -0400

    remove empty factory.rs

--- avenger-chart/src/scales/factory.rs
+++
@@ -1,3 +0,0 @@
-// This module previously contained scale factory functions.
-// The scale factory pattern has been removed in favor of direct instantiation
-// and the ScaleImpl trait's default_options method.
