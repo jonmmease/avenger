b367f0228056d0e05115aca9af7057a8bc6ca105
Jon Mease <jonmmease@gmail.com>
2026-01-31 10:54:12 -0500

    remove comment numbering

--- avenger-chart/src/facet/guide/measurement.rs
+++ avenger-chart/src/facet/guide/measurement.rs
@@ -1,15 +1,9 @@
-//! Measurement utilities for facet guides (stubbed)
+//! Measurement utilities for facet guides
 //!
-//! This module is stubbed as part of the facet fresh start refactoring.
-//! The complex two-pass measurement logic has been removed to allow
-//! rebuilding the facet system on the EvaluatedFacetTree abstraction.
+//! This module provides types for asynchronous overflow measurement.
 
 use crate::error::AvengerChartError;
-use crate::guide::{MeasurementResult, OverflowSpaceRequirement};
-use avenger_scales::scales::ConfiguredScale;
-use datafusion::prelude::SessionContext;
-use indexmap::IndexMap;
-use std::collections::HashMap;
+use crate::guide::OverflowSpaceRequirement;
 use std::future::Future;
 use std::pin::Pin;
 
@@ -19,33 +13,3 @@ pub type AsyncMeasureOverflowFn = Box<
         + Send
         + Sync,
 >;
-
-/// Trait for async measure_overflow function to enable generic callback
-#[allow(async_fn_in_trait)]
-pub trait MeasureOverflowCallback {
-    async fn call(
-        &self,
-        scales: &HashMap<String, ConfiguredScale>,
-        own_overflow: Option<&Vec<OverflowSpaceRequirement>>,
-        other_overflow: Option<&Vec<OverflowSpaceRequirement>>,
-        plot_width: f32,
-        plot_height: f32,
-        theme: &crate::theme::Theme,
-        params: &IndexMap<String, datafusion::common::ScalarValue>,
-        data_override: Option<&datafusion::dataframe::DataFrame>,
-        ctx: &SessionContext,
-    ) -> Result<OverflowSpaceRequirement, AvengerChartError>;
-}
-
-/// Stubbed measurement implementation
-///
-/// Returns default overflow as a placeholder. The full measurement
-/// logic will be rebuilt using the EvaluatedFacetTree.
-#[allow(clippy::too_many_arguments)]
-pub async fn measure_with_coordination_stub(
-    _plot_width: f32,
-    _plot_height: f32,
-) -> Result<MeasurementResult, AvengerChartError> {
-    // Return default measurement result
-    Ok(MeasurementResult::default())
-}
