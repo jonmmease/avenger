aee08f1d812579d5ddac6cb5587b43c3095991e8
Jon Mease <jonmmease@gmail.com>
2026-07-19 20:38:26 -0400

    chore(lang): complete compiler phase 10

--- avenger-lang-compiler/src/lib.rs
+++ avenger-lang-compiler/src/lib.rs
@@ -39,4 +39,4 @@ pub use schema::{LanguageHost, SemanticJsonSchema};
 pub use source_loader::{DefaultSourceLoader, SourceLoaderLimits};
 
 /// Last fully implemented language/compiler plan phase.
-pub const COMPILER_PHASE: u8 = 9;
+pub const COMPILER_PHASE: u8 = 10;
