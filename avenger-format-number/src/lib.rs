c7e78f0426888e4d720ea82fe509a062e735b6c6
Jon Mease <jonmmease@gmail.com>
2026-09-24 19:36:44 -0400

    refactor(format): rename formatted number module

--- avenger-format-number/src/lib.rs
+++ avenger-format-number/src/lib.rs
@@ -5,19 +5,19 @@ mod decimal;
 mod digits;
 mod error;
 mod format;
+mod formatted_number;
 mod locale;
 mod parser;
 mod registry;
 mod spec;
-mod typesetting;
 
 pub use adapters::{
     prepare_number_float_format, prepare_number_prefix_format, prepare_number_step_format,
 };
 pub use error::{FormatError, ParseError};
 pub use format::{format_number, NumberFormatOverrides, PreparedNumberFormat};
+pub use formatted_number::{FormattedNumber, NumberTypesetting};
 pub use locale::{LocaleId, NumberLocaleSpec, ResolvedNumberLocale};
 pub use parser::parse_number_spec;
 pub use registry::NumberLocaleRegistry;
 pub use spec::{Align, DigitSpec, FormatType, NumberFormatSpec, SignPolicy, Symbol};
-pub use typesetting::{FormattedNumber, NumberTypesetting};
