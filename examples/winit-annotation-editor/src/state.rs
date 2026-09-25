fc3556bc9d928a5f2b682832baa65b4d22e3b77c
Jon Mease <jonmmease@gmail.com>
2026-09-25 07:46:06 -0400

    refactor: forward shared number formatter configuration

--- examples/winit-annotation-editor/src/state.rs
+++ examples/winit-annotation-editor/src/state.rs
@@ -217,11 +217,8 @@ pub fn text_config() -> TextMeasurementConfig<'static> {
         font_style: FontStyle::Normal,
         syntax_mode: TextSyntaxMode::Plain,
         params: PARAMS.get_or_init(Default::default),
-        number_locale: None,
-        number_locale_specs: None,
-        datetime_locale: None,
-        datetime_timezone: None,
-        datetime_locale_specs: None,
+        number_format: None,
+        datetime_format: None,
     }
 }
 
